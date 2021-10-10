4e2d2f670b1c958a977ec7ec73ea5cabcaf80f45
Salomone Baquis <salomone.l.baquis@gmail.com>
2021-10-10 17:31:17 -0400

    Use padding for picture element aspect ratios

--- src/shortcodes/image.js
+++ src/shortcodes/image.js
@@ -4,7 +4,7 @@ const Image = require("@11ty/eleventy-img");
 function generateClassStr(classes) {
   if (!classes.length) return "";
 
-  return ` class="${classes}"`;
+  return;
 }
 
 // https://www.11ty.dev/docs/plugins/image/#filter-diy-picture-btn
@@ -36,7 +36,12 @@ module.exports = async function (src, alt, classes = "", sizes = "100vw") {
   const srcSet = (imageFormat) =>
     imageFormat.map((entry) => entry.srcset).join(", ");
 
-  return `<picture${generateClassStr(classes)}>
+  let classStr = "";
+  if (classes.length) {
+    classStr += ` class="${classes}"`;
+  }
+
+  return `<picture${classStr}>
     ${Object.values(metadata)
       .map((imageFormat) => {
         return `
