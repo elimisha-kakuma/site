b2e03bdfcadafe97fa9b4ef9748465acb7ea8f2b
Salomone Baquis <salomone.l.baquis@gmail.com>
2022-06-11 10:42:08 -0400

    New pic of Dudi

--- src/_data/staff.js
+++ src/_data/staff.js
@@ -18,7 +18,7 @@ const founders = [
   {
     name: "Joseph Dudi Miabok (Dudi)",
     pronouns: "he/him/his",
-    image: "/images/staff/dudi.jpeg",
+    image: "/images/staff/dudi.png",
     bio:
       "<p>Dudi is a South Sudanese refugee who grew up in Kakuma Refugee Camp, Kenya. He is a rising senior at Harvard University studying History and Science with a focus on Information, Technology and Society and also minors in Computer Science. He is passionate about creating access to quality education, one of the UN’s sustainable development goals (SDGs), to the less fortunate, especially refugees and internally displaced persons through the application of modern technology. He also went to and taught in primary and secondary schools in Kakuma. Therefore as a former teacher and student, he knows firsthand what it means to be a refugee with dreams and not have the opportunity to pursue them. This summer, he will be running a project that focuses on teaching 9th and 10th graders computer programming as well as teaching them how to apply to universities abroad after graduating from high school. By being a part of Elimisha Kakuma, Dudi hopes to realize his dreams of creating greater access to higher education for refugees.</p>",
   },
