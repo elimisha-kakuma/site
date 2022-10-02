baf05b64bf6d4f3353b5e80457b3a88775b0f96a
Salomone Baquis <salomone.l.baquis@gmail.com>
2022-10-02 16:48:23 -0400

    More accordion updates

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -3,6 +3,38 @@ class ReadMore extends HTMLElement {
     super();
 
     const self = this;
+
+    if (!document.querySelector("#read-more-styles")) {
+      // If load the styles via an external stylesheet, we can't reliably get the clientHeight/scrollHeight of the paragraphs, which makes it difficult to know whether we should add the More/Less button.
+      const styles = `
+      <style id="read-more-styles">
+        read-more {
+          display: block;
+        }
+        read-more .read-more__button {
+          --flow-space: var(--s-1);
+          background: none;
+          border: none;
+          color: inherit;
+          cursor: pointer;
+          font-weight: bold;
+          padding-left: 0;
+          padding-right: 0;
+          position: relative;
+          text-decoration: underline;
+        }
+        read-more:not(.expanded) *:first-child {
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 5;
+          overflow: hidden;
+        }
+        read-more:not(.expanded) * + *:not(.read-more__button) {
+          display: none;
+        }
+      </style>`;
+      document.head.insertAdjacentHTML("beforeend", styles);
+    }
     const initialState = {
       expanded: false,
     };
@@ -21,7 +53,7 @@ class ReadMore extends HTMLElement {
         state[key] = value;
 
         if (oldValue !== value) {
-          self.handleStateChange();
+          self.handleStateChange(key, value);
         }
         return true;
       },
@@ -29,14 +61,25 @@ class ReadMore extends HTMLElement {
   }
 
   connectedCallback() {
-    this.initialMarkup = this.innerHTML;
-    this.innerHTML = `
-      ${this.initialMarkup}
-      <button class="read-more__button">More [+]</button>
-    `;
+    this.render();
+  }
 
+  getButtonText() {
+    return this.state.expanded ? "Less [-]" : "More [+]";
+  }
+
+  render() {
+    let renderedHtml = this.innerHTML;
+    // If the scrollHeight of the first paragraph is greater than the clientHeight (the line-clamped height), show the button.
+    const hasButton =
+      this.children[0]?.scrollHeight > this.children[0]?.clientHeight;
+    if (hasButton) {
+      renderedHtml = `${renderedHtml}<button class="read-more__button">${this.getButtonText()}</button>`;
+    }
+    this.innerHTML = renderedHtml;
     this.postRender();
   }
+
   postRender() {
     this.readMoreButton = this.querySelector(".read-more__button");
     this.addEventListener("click", (e) => {
@@ -44,14 +87,19 @@ class ReadMore extends HTMLElement {
       this.state.expanded = !this.state.expanded;
     });
   }
-  handleStateChange() {
-    const isExpanded = this.state.expanded;
-    if (isExpanded) {
-      this.readMoreButton.textContent = "Less [-]";
-      this.classList.add("expanded");
-    } else {
-      this.readMoreButton.textContent = "More [+]";
-      this.classList.remove("expanded");
+
+  handleStateChange(key, val) {
+    switch (key) {
+      case "expanded":
+        if (this.state.expanded) {
+          this.classList.add("expanded");
+        } else {
+          this.classList.remove("expanded");
+        }
+        this.readMoreButton.textContent = this.getButtonText();
+        break;
+      default:
+        return;
     }
   }
 }
